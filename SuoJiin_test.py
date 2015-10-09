6d1b062afdc3b75a870523a2ae29e7df6840630f
lihuating <lihuating0722@163.com>
2015-10-09 22:12:44 +0800

    aa

---
+++ SuoJiin_test.py
@@ -0,0 +1,8 @@
+
+a = 100 
+if a > 10:
+	print 'good'
+	print 'over'
+if a < 10:
+	print 'bad'
+	print 'over'
---
+++ hello_lht.py
@@ -0,0 +1,5 @@
+#hello_lht.py
+#这个代码是用来表明输入输入如何控制的
+
+value = raw_input('input your name:')
+print "hello", value
